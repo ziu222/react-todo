17487318ba1cf8e8369b565b9e35deebaa5d56ec
DESKTOP-GSPLG6E\Nghia <btn2812@gmail.com>
2026-05-05 21:35:33 +0700

    init

--- to-do app/eslint.config.js
+++ to-do app/eslint.config.js
@@ -7,7 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
